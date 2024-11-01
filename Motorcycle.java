fbfa16b71a1d5755b88798e6d253ac0f9238f7a5
saurabhsingh <saurabh623a@gmail.com>
2024-11-01 23:16:14 +0530

    Initial Code for parkinglot

---
+++ Motorcycle.java
@@ -0,0 +1,15 @@
+public class Motorcycle extends Vehicle{
+    public Motorcycle() {
+        spotsNeeded = 1;
+        size = VehicleSize.Motorcycle;
+    }
+    @Override
+    public boolean canFitInSpot(ParkingSpot spot) {
+        return true;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("M");
+    }
+}
---
+++ parkinglot/Readme.md
@@ -0,0 +1,9 @@
+# Design a parking lot
+
+#### Follow-up questions
+1. What type of vehicles are supported?
+2. How many floors?
+
+#### Assumptions
+
+
