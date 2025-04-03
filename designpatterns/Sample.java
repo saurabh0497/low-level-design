4aa1102485c71c121a8a9a28d0d2b960ff4c041c
saurabhsingh <saurabh0497@gmail.com>
2025-04-03 21:23:55 +0530

    Day-2

---
+++ designpatterns/Sample.java
@@ -0,0 +1,22 @@
+package designpatterns;
+
+public class Sample {
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public static void setTime(String time) {
+        this.time = time;
+    }
+
+    private static String data;
+    private static String time;
+
+
+
+    public static void main(String[] args) {
+        Sample sample = new Sample();
+        sample.setData();
+    }
+
+}
